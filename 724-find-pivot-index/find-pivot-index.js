d87944d1b1f8d42c4b05cfa2979c17443ff8c033
Hank <57084183+vineetsgr07@users.noreply.github.com>
2024-05-07 13:07:24 -0700

    Time: 59 ms (84.08%) | Memory: 52.2 MB (39.49%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    
+    let totalSum = nums.reduce((acc, val) => acc + val, 0)
+    
+    let leftSum = 0
+    
+    for(let i = 0; i < nums.length; i++){
+        
+        if(leftSum == totalSum - leftSum - nums[i]){
+            return i
+        }
+        
+        leftSum += nums[i]
+        
+    }
+
+    return -1
+};
