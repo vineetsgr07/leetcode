e68b6246dc40861c76de6d95ad69fb44bde514f5
Hank <57084183+vineetsgr07@users.noreply.github.com>
2024-05-07 16:31:15 -0700

    Time: 90 ms (77.64%) | Memory: 58.9 MB (53.69%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    
+    intervals.sort((a, b) => a[0] - b[0]);
+    const merged = [];
+
+    for(let i = 0; i<intervals.length; i++){
+
+        if (merged.length === 0 || merged[merged.length-1][1] < intervals[i][0]){
+            merged.push(intervals[i])
+        } else {
+            merged[merged.length-1][1] = Math.max(merged[merged.length - 1][1], intervals[i][1]);
+        }
+    }
+
+    return merged
+};
