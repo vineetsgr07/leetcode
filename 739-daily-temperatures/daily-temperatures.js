2956571e9fccab6cfeff3289637b76995dec42b6
Hank <57084183+vineetsgr07@users.noreply.github.com>
2024-05-08 12:47:53 -0700

    Time: 213 ms (60.47%) | Memory: 75.5 MB (29.74%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -3,20 +3,23 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    const n = temperatures.length
-    const answer = Array(n).fill(0)
-    const stack = []
+    let n = temperatures.length
+    let answer = new Array(n).fill(0)
+    let stack=[]
 
-    for(let i = n - 1; i>=0; i--){
-        while (stack.length > 0 && temperatures[stack[stack.length - 1]] <= temperatures[i]) {
-            stack.pop();
-        }
-
-        if(stack.length > 0){
-            answer[i] = stack[stack.length - 1] - i
+    for(let i=n-1; i>=0; i--){   
+        
+        while(stack.length > 0 && temperatures[stack[stack.length-1]] <= temperatures[i]){
+            stack.pop()
         }
         
+        if(stack.length > 0){
+            answer[i] = stack[stack.length-1] - i
+        }
+
         stack.push(i)
     }
+
     return answer
+
 }
