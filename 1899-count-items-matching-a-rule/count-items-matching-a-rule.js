02b585cbf8af0ba6a04bf3fc237a1f49d5f627b8
Hank <57084183+vineetsgr07@users.noreply.github.com>
2024-05-08 17:24:35 -0700

    Time: 62 ms (76.56%) | Memory: 53.4 MB (27.95%) - LeetSync

--- 1899-count-items-matching-a-rule/count-items-matching-a-rule.js
+++ 1899-count-items-matching-a-rule/count-items-matching-a-rule.js
@@ -4,30 +4,20 @@
  * @param {string} ruleValue
  * @return {number}
  */
-var countMatches = function(items, ruleKey, ruleValue) {
-    
+var countMatches = function (items, ruleKey, ruleValue) {
+
     let count = 0;
-    for(let item of items){
-        let type = item[0]
-        let color = item[1]
-        let name = item[2]
-
-        if(ruleKey == "type"){
-            if(ruleValue == type){
-                count+=1
-            }
+    for (let item of items) {
+        if (ruleKey == "type" && ruleValue == item[0]) {
+            count += 1
         }
 
-        if(ruleKey == "color"){
-            if(ruleValue == color){
-                count+=1
-            }
+        if (ruleKey == "color" && ruleValue == item[1]) {
+            count += 1
         }
 
-        if(ruleKey == "name"){
-            if(ruleValue == name){
-                count+=1
-            }
+        if (ruleKey == "name" && ruleValue == item[2]) {
+            count += 1
         }
     }
 
