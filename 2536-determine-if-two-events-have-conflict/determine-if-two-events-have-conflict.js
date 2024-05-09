0626413f59d49f3ac1d6e2cb19b57a00a07eee69
Hank <57084183+vineetsgr07@users.noreply.github.com>
2024-05-09 15:28:38 -0700

    Time: 41 ms (95.80%) | Memory: 49.3 MB (7.56%) - LeetSync

---
+++ 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string[]} event1
+ * @param {string[]} event2
+ * @return {boolean}
+ */
+
+const setHoursMinutes = (time) =>{
+    const [hours, minutes] = time.split(":")
+    const now = new Date()
+
+    now.setHours(hours, minutes, 0 ,0)
+    return now
+}
+
+var haveConflict = function(event1, event2) {
+    let splitedEvent11 = setHoursMinutes(event1[0])
+    let splitedEvent12 = setHoursMinutes(event1[1])
+
+    let splitedEvent21 = setHoursMinutes(event2[0])
+    let splitedEvent22 = setHoursMinutes(event2[1])
+
+    if(splitedEvent11 <= splitedEvent21 && splitedEvent21 <= splitedEvent12 ){
+        return true
+    }
+
+    if(splitedEvent11 <= splitedEvent22 && splitedEvent22 <= splitedEvent12 ){
+        return true
+    }
+
+    if(splitedEvent21 <= splitedEvent11 && splitedEvent12<=splitedEvent22){
+        return true
+    }
+
+    return false
+};
