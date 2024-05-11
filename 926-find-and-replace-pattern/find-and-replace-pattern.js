8ede02dd46083ca060ae996f31686574a975c3a8
Hank <57084183+vineetsgr07@users.noreply.github.com>
2024-05-11 16:17:56 -0700

    Time: 56 ms (59.24%) | Memory: 51.1 MB (65.61%) - LeetSync

--- 926-find-and-replace-pattern/find-and-replace-pattern.js
+++ 926-find-and-replace-pattern/find-and-replace-pattern.js
@@ -4,6 +4,7 @@
  * @return {string[]}
  */
 var findAndReplacePattern = function (words, pattern) {
+    let store = []
 
     const canReplace = (string1) => {
         let hash = new Map()
@@ -11,25 +12,17 @@ var findAndReplacePattern = function (words, pattern) {
         for (let i = 0; i < pattern.length; i++) {
             let toBeMapped = hash.get(pattern[i])
             if (toBeMapped) {
-                if (toBeMapped !== string1[i]) {
-                    return false
-                }
+                if (toBeMapped !== string1[i]) return false
             } else {
-                if(string1.slice(0, i).includes(string1[i])){
-                    return false
-                }
+                if(string1.slice(0, i).includes(string1[i])) return false
                 hash.set(pattern[i], string1[i])
             }
         }
         return true
     }
 
-    let store = []
-
-    for(let j=0; j<words.length;j++){
-        if(canReplace(words[j])){
-            store.push(words[j])
-        }
+    for(let j in words){
+        canReplace(words[j]) && store.push(words[j])
     }
 
     return store;
