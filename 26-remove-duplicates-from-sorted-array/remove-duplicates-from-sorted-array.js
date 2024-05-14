b3f0d7168220bd833228d572d4129110fe228fa5
Hank <57084183+vineetsgr07@users.noreply.github.com>
2024-05-14 02:21:09 -0700

    Time: 59 ms (80.45%) | Memory: 52.3 MB (45.98%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+
+    if(nums.length == 0){
+        return 0
+    }
+
+    let k = 1
+
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] !== nums[k - 1]){
+            nums[k] = nums[i]
+            k++
+        }
+    }
+
+    return k
+};
