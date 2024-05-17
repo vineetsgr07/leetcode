004bfaff7897a6e8686325e7bb108bbb5d24e16d
Hank <57084183+vineetsgr07@users.noreply.github.com>
2024-05-17 16:08:00 -0700

    Time: 57 ms (68.76%) | Memory: 52.4 MB (18.42%) - LeetSync

--- 837-most-common-word/most-common-word.js
+++ 837-most-common-word/most-common-word.js
@@ -6,34 +6,24 @@
 var mostCommonWord = function (paragraph, banned) {
     let hash = new Map()
     let array = ""
-    
-    for (let i = 0; i < paragraph.length; i++) {
-        let char = paragraph[i].toLowerCase().charCodeAt()
-        if (char >= 97 && char <= 122) {
-            array += paragraph[i].toLowerCase()
-        } else {
-            if(paragraph[i] == " "){
-                array += paragraph[i]
-            }else{
-                array += " "
-            }
-        }
+    let maxOccurance = 0;
+    let element = ""
+
+    for (let para of paragraph) {
+        let char = para.toLowerCase().charCodeAt()
+        array += (char >= 97 && char <= 122) ? para.toLowerCase() : " "
     }
 
-    let convertToArray = array.split(" ").filter(x => x)
-    let maxOccurance = 0;
-    let foundAtIndex = -1
-
-    for (let item of convertToArray) {
-        if (!banned.includes(item)) {
+    for (let item of array.split(" ")) {
+        if (!banned.includes(item) && item) {
             let currentCout = (hash.get(item) || 0) + 1
             hash.set(item, currentCout)
             if (currentCout > maxOccurance) {
                 maxOccurance = currentCout
-                foundAtIndex = item
+                element = item
             }
         }
     }
 
-    return foundAtIndex
+    return element
 };
