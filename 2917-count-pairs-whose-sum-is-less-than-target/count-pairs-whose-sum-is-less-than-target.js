b782f96c382c283c7c96169a1204d014348ed59a
Hank <57084183+vineetsgr07@users.noreply.github.com>
2024-05-20 12:25:37 -0700

    Time: 98 ms (5.03%) | Memory: 53.1 MB (5.58%) - LeetSync

---
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function (nums, target) {
+    nums.sort((a, b) => a - b)
+
+    let first = 0
+    let last = nums.length - 1
+    let count = 0
+    console.log(nums)
+    while (first < last) {
+        if (nums[first] + nums[last] < target) {
+            count += last-first
+            first++
+        } else {
+            last--
+        }
+    }
+    return count
+
+};
