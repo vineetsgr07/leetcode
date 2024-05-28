a233c6c57a0ff7fc439247c136e95cce960029e7
Hank <57084183+vineetsgr07@users.noreply.github.com>
2024-05-28 13:44:50 -0700

    Time: 59 ms (66.39%) | Memory: 51.4 MB (62.58%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -6,17 +6,9 @@ var majorityElement = function (nums) {
 
     let size = nums.length / 2
     let store = new Map()
-    let count = 0
-    let max = 0
 
     for (let i = 0; i < nums.length; i++) {
         store.set(nums[i], (store.get(nums[i]) || 0) + 1)
-
-        if (store.get(nums[i]) > size) {
-            return nums[i]
-        }
+        if (store.get(nums[i]) > size) return nums[i]
     }
-
 };
-
-// 7/2 = 3.5
