64a755d578241174b3b41ee3aca47f9cbf3fe4e3
Hank <57084183+vineetsgr07@users.noreply.github.com>
2024-06-01 21:45:17 -0700

    Time: 110 ms (13.79%) | Memory: 61.5 MB (91.46%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.ts
@@ -0,0 +1,22 @@
+function removeStars(s: string): string {
+
+    let count = 0
+    let n = s.length - 1
+
+    let arr = s.split("")
+
+    for (let i = n; i >= 0; i--) {
+
+        if (arr[i] === "*") {
+            count++
+        }
+
+        if (arr[i] !== "*" && count > 0) {
+            arr[i] = ""
+            count--
+        }
+    }
+
+    return arr.join("").replaceAll("*","")
+
+};
